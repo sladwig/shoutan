2cc7a22bfd22bdba54518ce99b86db945b735a7a
Stefan Ladwig <stefanl@dwig.dev>
2020-10-29 16:05:46 +0100

    refactor: add migration extension

--- db/migrate/20201026152807_create_games.rb
+++ db/migrate/20201026152807_create_games.rb
@@ -1,5 +1,8 @@
 class CreateGames < ActiveRecord::Migration[6.0]
   def change
+	enable_extension 'uuid-ossp'
+    enable_extension 'pgcrypto'
+
     create_table :games, id: :uuid do |t|
       t.text :sgf
 
