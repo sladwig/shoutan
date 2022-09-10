c447bd1204f41e6cc6372a036768d55109849372
Stefan Ladwig <sl@dwig.dev>
2022-09-10 22:45:20 +0200

    chore: update ruby version, readme and schema

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Shoutan
 
-Shoutan is a websocket go game server.
+Shoutan is a websocket go (game) server.
 
 It's in a very early stage, but works sofar.
+
+## setup
+
+```
+bundle
+rails db:create
+rails db:migrate
+```
+
+run with:
+
+`rails s`
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,9 @@
 ActiveRecord::Schema.define(version: 2020_10_26_152807) do
 
   # These are extensions that must be enabled in order to support this database
+  # enable_extension "pgcrypto"
   enable_extension "plpgsql"
+  # enable_extension "uuid-ossp"
 
   create_table "games", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.text "sgf"
