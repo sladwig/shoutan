7186065bd73c72a9525b1ab0ad47abc72bbab082
Stefan Ladwig <sl@dwig.dev>
2022-09-10 23:56:46 +0200

    chore: upgraded rails

--- db/schema.rb
+++ db/schema.rb
@@ -2,25 +2,16 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_152807) do
-
-  # These are extensions that must be enabled in order to support this database
-  # enable_extension "pgcrypto"
-  enable_extension "plpgsql"
-  # enable_extension "uuid-ossp"
-
-  create_table "games", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.text "sgf"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema[7.0].define(version: 2022_09_10_214258) do
+# Could not dump table "games" because of following StandardError
+#   Unknown type 'uuid' for column 'id'
 
 end
